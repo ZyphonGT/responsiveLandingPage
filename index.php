5bb0d0d98adb2e824bd684a90139c1ee046f76e4
ZyphonGT <rickygani10@gmail.com>
2019-05-14 00:48:57 +0700

    Finished Banner

--- index.php
+++ index.php
@@ -21,27 +21,35 @@
     </header>
 
     <div id="main">
-        <div class="border">
-            Banner here
+        <!-- BANNER -->
+        <div class="banner-wrapper">
+            <img src="res/img/banner.jpg" alt="Banner">
+            <div class="banner-caption">
+                <h1>WE BUILD<br>AND<br>SOLVE THINGS</h1>
+            </div>
         </div>
+        
 
+        <!-- THREE CARDS (welcome) -->
         <div class="border">
             Welcome to our company
         </div>
 
+        <!-- THREE CARDS (Product) -->
         <div class=border> 
             Product
         </div>
 
+        <!-- Services and Three Logo -->
         <div class=border> 
             Service
         </div>
-
-        <div class=border> 
-            Contact
-        </div>
-
     </div>
+
+    <!-- contact us -->
+    <footer>
+        CONTACT
+    </footer>
     
 </body>
 
