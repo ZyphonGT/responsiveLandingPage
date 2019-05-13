97a93ee2354fa0ec0592b31cf485520439217f8e
ZyphonGT <rickygani10@gmail.com>
2019-05-13 23:40:25 +0700

    Initial Commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<div class="header">
+    <div class="logo-icon">
+        <img src="res/img/title.png" alt="3iLogo">
+        
+        <!-- https://via.placeholder.com/450x1000.png-->
+    </div>
+
+    <nav>
+        <!-- HOME PRODUCT SERVICE CONTACT ABOUT -->
+        <ul>
+            <a href="#a"><li>HOME</li></a>
+            <a href="#b"><li>PRODUCT</li></a>
+            <a href="#c"><li>SERVICE</li></a>
+            <a href="#d"><li>CONTACT</li></a>
+            <a href="#e"><li>ABOUT</li></a>
+        </ul>
+    </nav>
+</div>
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>iii</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="style/index.css">
+    <link rel="stylesheet" href="style/header.css"> <!-- HEADER -->
+</head>
+
+<body>
+    <header> 
+        <?php
+            require 'header.php';
+        ?>
+    </header>
+
+    <div id="main">
+        <div class="border">
+            Banner here
+        </div>
+
+        <div class="border">
+            Welcome to our company
+        </div>
+
+        <div class=border> 
+            Product
+        </div>
+
+        <div class=border> 
+            Service
+        </div>
+
+        <div class=border> 
+            Contact
+        </div>
+
+    </div>
+    
+</body>
+
+</html>
+
