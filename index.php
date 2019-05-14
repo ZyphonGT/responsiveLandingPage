c4d3fca4632c3b5ed6512b463d65f3b260caa67d
ZyphonGT <rickygani10@gmail.com>
2019-05-14 12:05:58 +0700

    finished "Welcome Section"

--- index.php
+++ index.php
@@ -25,14 +25,40 @@
         <div class="banner-wrapper">
             <img src="res/img/banner.jpg" alt="Banner">
             <div class="banner-caption">
-                <h1>WE BUILD<br>AND<br>SOLVE THINGS</h1>
+                WE BUILD<br>AND<br>SOLVE THINGS
             </div>
         </div>
         
 
         <!-- THREE CARDS (welcome) -->
-        <div class="border">
-            Welcome to our company
+        <div class="welcome-wrapper">
+            <h1 id="title">WELCOME TO OUR COMPANY</h1>
+            <h1 id="subtitle">
+                <span class="underline">WE BRING MAINTENANCE TO THE NEXT LEVEL</span>
+            </h2>
+
+            <div class="cardIMG-wrapper">
+                <div class="cardIMG-item">
+                    <div class="cardIMG" id="cardIMG-one">
+                        <div class="cardIMG-caption"> TECHNOLOGY </div>
+                    </div>
+                    <p>Vibsense delivers the earliest and most accurate warning of equipment failures and prescribes detailed actions to mitigate risks and solve problems.</p>
+                </div>
+
+                <div class="cardIMG-item">
+                    <div class="cardIMG" id="cardIMG-two">
+                        <div class="cardIMG-caption"> QUALITY </div>
+                    </div>
+                    <p>Vibsense delivers the earliest and most accurate warning of equipment failures and prescribes detailed actions to mitigate risks and solve problems.</p>
+                </div>
+
+                <div class="cardIMG-item">
+                    <div class="cardIMG" id="cardIMG-three">
+                        <div class="cardIMG-caption"> THE STAFF </div>
+                    </div>
+                    <p>Vibsense delivers the earliest and most accurate warning of equipment failures and prescribes detailed actions to mitigate risks and solve problems.</p>
+                </div>
+            </div>
         </div>
 
         <!-- THREE CARDS (Product) -->
